604559add2c53496237b96dcab61c0315602b181
RandikaBatawala <randka.batawala99@gmail.com>
2023-10-05 00:43:46 +0530

    feat: add basic structure

---
+++ server.js
@@ -0,0 +1,37 @@
+// Importing required modules
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+
+// Loading environment variables
+require("dotenv").config({
+	path: path.resolve(__dirname, ".env"),
+});
+
+// Constants
+const PORT = process.env.PORT || 8070;
+const URL = process.env.MONGODB_URL;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect(URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+
+// Event listener for successful MongoDB connection
+connection.once("open", () => {
+	console.log("MongoDB connection successful!");
+});
+
+// Start the server
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
